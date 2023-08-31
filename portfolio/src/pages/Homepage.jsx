f2fffbb5847e38ae995d2d814fb2d89bedd04218
florian autricque <fautricque@gmail.com>
2023-08-31 12:53:44 -0400

    hompage done

--- portfolio/src/pages/Homepage.jsx
+++ portfolio/src/pages/Homepage.jsx
@@ -1,13 +1,16 @@
 import PageNav from "../components/PageNav";
+import styles from "./Homepage.module.css";
 
 function Homepage() {
   return (
     <>
       <PageNav />
-      <h1>
-        Front-end Developer <br />
-        Creative
-      </h1>
+      <div className={styles.container}>
+        <h1 className={styles.homepage}>
+          <span>Front-end </span>Developer <br />
+          Creative
+        </h1>
+      </div>
     </>
   );
 }
