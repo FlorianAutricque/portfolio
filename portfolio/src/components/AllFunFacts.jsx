f36981766675855a0432c26e67edbe9cec70ff2b
florian autricque <fautricque@gmail.com>
2023-09-02 17:07:05 -0400

    reveal infos fun facts

--- portfolio/src/components/AllFunFacts.jsx
+++ portfolio/src/components/AllFunFacts.jsx
@@ -3,6 +3,7 @@ import Box from "./Box";
 import { FaPeopleGroup } from "react-icons/fa6";
 import { FaSkiing } from "react-icons/fa";
 import styles from "./SizeBox.module.css";
+import styles2 from "./AllFunFacts.module.css";
 
 function AllFunFacts() {
   return (
@@ -11,32 +12,41 @@ function AllFunFacts() {
         icon={<GiPencilBrush size={70} />}
         size1={styles.large}
         size2={styles.mediumLarge}
+        reveal={styles2.reveal}
       >
         <h3>Art</h3>
-        As an art enthusiast, drawing is, to me, an expression of my creativity.
-        I like to think of coding the same way as drawing, just substituting a
-        pen with letters and numbers.
+        <p className={styles2.text}>
+          As an art enthusiast, drawing is, to me, an expression of my
+          creativity. I like to think of coding the same way as drawing, just
+          substituting a pen with letters and numbers.
+        </p>
       </Box>
 
       <Box
         icon={<FaPeopleGroup size={70} />}
         size1={styles.large}
         size2={styles.mediumLarge}
+        reveal={styles2.reveal}
       >
         <h3>Management</h3>
-        I've supervised 150+ individuals on Australian solar plant projects,
-        highlighting the importance of teamwork to me.
+        <p className={styles2.text}>
+          I've supervised 150+ individuals on Australian solar plant projects,
+          highlighting the importance of teamwork to me.
+        </p>
       </Box>
 
       <Box
         icon={<FaSkiing size={70} />}
         size1={styles.large}
         size2={styles.mediumLarge}
+        reveal={styles2.reveal}
       >
         <h3>Sport</h3>
-        Sports hold a special place in my heart, with skiing being a true
-        passion. During my younger years, I even competed as a semi-professional
-        skier.
+        <p className={styles2.text}>
+          Sports hold a special place in my heart, with skiing being a true
+          passion. During my younger years, I even competed as a
+          semi-professional skier.
+        </p>
       </Box>
     </>
   );
--- portfolio/src/components/Box.jsx
+++ portfolio/src/components/Box.jsx
@@ -1,8 +1,8 @@
 import styles1 from "./Box.module.css";
 
-function Box({ children, icon, size1, size2 }) {
+function Box({ children, icon, size1, size2, reveal }) {
   return (
-    <div className={`${styles1.boxFunFact} ${size1}`}>
+    <div className={`${styles1.boxFunFact} ${size1} ${reveal}`}>
       <div className={`${styles1.boxOutside} ${size2}`}>
         {icon}
         {children}
