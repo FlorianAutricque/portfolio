ec9584b532bb6b2d89999e26c7927c503860a71f
florian autricque <fautricque@gmail.com>
2023-09-02 16:47:10 -0400

    refactoring

---
+++ portfolio/src/components/VerticalLineExperiences.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import styles from "./VerticalLineExperiences.module.css";
+
+function VerticalLineExperiences() {
+  const [scrollHeight, setScrollHeight] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY || document.documentElement.scrollTop;
+      setScrollHeight(scrollTop);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  return (
+    <div
+      className={styles.horizontal}
+      style={{ height: `${scrollHeight + 400}px` }}
+    ></div>
+  );
+}
+
+export default VerticalLineExperiences;
