95f38d285dab23f00f3f6af1f6e7f84feeb661f6
florian autricque <fautricque@gmail.com>
2023-09-03 13:55:18 -0400

    routes and images project

--- portfolio/src/pages/Project.jsx
+++ portfolio/src/pages/Project.jsx
@@ -1,33 +1,72 @@
-import { Link, useParams } from "react-router-dom";
+// import { Link, useParams } from "react-router-dom";
+// import PageNav from "../components/PageNav";
+// import styles from "../components/Button.module.css";
+
+// function Project() {
+//   const { projectId } = useParams();
+
+//   const projects = {
+//     1: {
+//       projectId: 1,
+//       name: "coucou",
+//     },
+//     2: {
+//       projectId: 2,
+//       name: "caca",
+//     },
+//   };
+
+//   const project = projects[projectId];
+
+//   return (
+//     <>
+//       <PageNav />
+//       <div>
+//         <h1>Project Details</h1>
+//         <p>Project ID: {project.projectId}</p>
+//         <p>Project Name: {project.name}</p>
+//       </div>
+
+//       <Link to="/projects" className={styles.button}>
+//         Go back
+//       </Link>
+//     </>
+//   );
+// }
+
+// export default Project;
+
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
 import PageNav from "../components/PageNav";
-import Button from "../components/Button";
+import styles from "../components/Button.module.css";
 
 function Project() {
-  const { projectId } = useParams();
+  const location = useLocation();
 
-  const projects = {
-    1: {
-      projectId: 1,
-      name: "coucou",
-    },
-    2: {
-      projectId: 2,
-      name: "caca",
-    },
-  };
-
-  const project = projects[projectId];
+  const project = location.state ? location.state.projectData : null;
 
   return (
     <>
       <PageNav />
       <div>
         <h1>Project Details</h1>
-        <p>Project ID: {project.projectId}</p>
-        <p>Project Name: {project.name}</p>
+        <p>Project ID: {project ? project.projectId : ""}</p>
+        <p>Project Name: {project ? project.name : ""}</p>
+
+        {project
+          ? project.mainImage && (
+              <p>
+                Project image:
+                <img src={project.mainImage} alt={project.name} />
+              </p>
+            )
+          : ""}
       </div>
 
-      <Link to="/projects">Go back</Link>
+      <Link to="/projects" className={styles.button}>
+        Go back
+      </Link>
     </>
   );
 }
--- portfolio/src/pages/Projects.jsx
+++ portfolio/src/pages/Projects.jsx
@@ -1,13 +1,53 @@
+// import { NavLink } from "react-router-dom";
+// import PageNav from "../components/PageNav";
+
+// function Projects() {
+//   return (
+//     <div>
+//       <PageNav />
+
+//       <NavLink to="/project/:projectId">Project 1</NavLink>
+//       <NavLink to="/project/2">Project 2</NavLink>
+//     </div>
+//   );
+// }
+
+// export default Projects;
+
+import React from "react";
 import { NavLink } from "react-router-dom";
 import PageNav from "../components/PageNav";
 
 function Projects() {
+  const projects = [
+    {
+      projectId: 1,
+      name: "Project 1",
+      mainImage: "/projectsImages/worldwide/worldwide1.png",
+    },
+    {
+      projectId: 2,
+      name: "Project 2",
+      mainImage: "/projectsImages/weatherApp/weatherapp1.png",
+    },
+  ];
+
   return (
     <div>
       <PageNav />
 
-      <NavLink to="/project/1">Project 1</NavLink>
-      <NavLink to="/project/2">Project 2</NavLink>
+      {projects.map(project => (
+        <NavLink
+          key={project.projectId}
+          to={`/project/${project.projectId}`}
+          state={{ projectData: project }}
+        >
+          {project.name}
+          {project.mainImage && (
+            <img src={project.mainImage} alt={project.name} />
+          )}
+        </NavLink>
+      ))}
     </div>
   );
 }
