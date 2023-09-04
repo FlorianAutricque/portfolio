b0e8f3664efb18dbed2fd8a72f4193ce5523256d
florian autricque <fautricque@gmail.com>
2023-09-04 13:50:39 -0400

    projects creation and data

--- portfolio/src/pages/Projects.jsx
+++ portfolio/src/pages/Projects.jsx
@@ -1,34 +1,81 @@
-// import { NavLink } from "react-router-dom";
-// import PageNav from "../components/PageNav";
-
-// function Projects() {
-//   return (
-//     <div>
-//       <PageNav />
-
-//       <NavLink to="/project/:projectId">Project 1</NavLink>
-//       <NavLink to="/project/2">Project 2</NavLink>
-//     </div>
-//   );
-// }
-
-// export default Projects;
-
 import React from "react";
 import { NavLink } from "react-router-dom";
 import PageNav from "../components/PageNav";
+import styles from "./Projects.module.css";
+import styles2 from "../components/SizeBox.module.css";
+import Box from "../components/Box";
 
 function Projects() {
   const projects = [
     {
       projectId: 1,
-      name: "Project 1",
-      mainImage: "/projectsImages/worldwide/worldwide1.png",
+      name: "WorldWise",
+      mainImage: "/projectsImages/worldwise/worldwise1.png",
     },
     {
       projectId: 2,
-      name: "Project 2",
+      name: "Weather App",
       mainImage: "/projectsImages/weatherApp/weatherapp1.png",
+    },
+    {
+      projectId: 3,
+      name: "OUVA",
+      mainImage: "/projectsImages/ouva/ouva1.png",
+    },
+    {
+      projectId: 4,
+      name: "Hairbnb",
+      mainImage: "/projectsImages/hairbnb/airbnb1.png",
+    },
+    {
+      projectId: 5,
+      name: "The Wild Oasis",
+      mainImage: "/projectsImages/wildoasis/wildoasis1.png",
+    },
+    {
+      projectId: 6,
+      name: "usePopcorn",
+      mainImage: "/projectsImages/usepopcorn/usepopcorn1.png",
+    },
+    {
+      projectId: 7,
+      name: "Fast React Pizza",
+      mainImage: "/projectsImages/pizzareact/pizzareact1.png",
+    },
+    {
+      projectId: 8,
+      name: "React Quizz",
+      mainImage: "/projectsImages/reactquizz/quizz1.png",
+    },
+    {
+      projectId: 9,
+      name: "Far Away",
+      mainImage: "/projectsImages/faraway/faraway1.png",
+    },
+    {
+      projectId: 10,
+      name: "Bankist",
+      mainImage: "/projectsImages/bankist/bankist1.png",
+    },
+    {
+      projectId: 11,
+      name: "Forkify",
+      mainImage: "/projectsImages/forkify/forkify1.png",
+    },
+    {
+      projectId: 12,
+      name: "Mapty",
+      mainImage: "/projectsImages/mapty/mapty1.png",
+    },
+    {
+      projectId: 13,
+      name: "Guess My Number",
+      mainImage: "/projectsImages/number/number1.png",
+    },
+    {
+      projectId: 14,
+      name: "Pig Game",
+      mainImage: "/projectsImages/piggame/game1.png",
     },
   ];
 
@@ -36,18 +83,30 @@ function Projects() {
     <div>
       <PageNav />
 
-      {projects.map(project => (
-        <NavLink
-          key={project.projectId}
-          to={`/project/${project.projectId}`}
-          state={{ projectData: project }}
-        >
-          {project.name}
-          {project.mainImage && (
-            <img src={project.mainImage} alt={project.name} />
-          )}
-        </NavLink>
-      ))}
+      <h1>List of projects</h1>
+
+      <div className={styles.containerProjects}>
+        {projects.map(project => (
+          <NavLink
+            key={project.projectId}
+            to={`/project/${project.projectId}`}
+            state={{ projectData: project }}
+          >
+            <div>
+              <Box size1={styles2.sizeProject}>
+                {project.name}
+                {project.mainImage && (
+                  <img
+                    src={project.mainImage}
+                    alt={project.name}
+                    className={styles.img}
+                  />
+                )}
+              </Box>
+            </div>
+          </NavLink>
+        ))}
+      </div>
     </div>
   );
 }
