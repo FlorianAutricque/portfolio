ba950de151aa7734f99d75eea956b973824f3142
florian autricque <fautricque@gmail.com>
2023-09-06 13:29:01 -0400

    responsive app

--- portfolio/src/components/AllFunFacts.jsx
+++ portfolio/src/components/AllFunFacts.jsx
@@ -8,52 +8,46 @@ import styles2 from "./AllFunFacts.module.css";
 function AllFunFacts() {
   return (
     <div className={styles2.containerFF}>
-      <div className={styles2.x}>
-        <Box
-          icon={<GiPencilBrush size={70} />}
-          size1={styles.large}
-          size2={styles.mediumLarge}
-          reveal={styles2.reveal}
-        >
-          <h3>Art</h3>
-          <p className={styles2.text}>
-            As an art enthusiast, drawing is, to me, an expression of my
-            creativity. I like to think of coding the same way as drawing, just
-            substituting a pen with letters and numbers.
-          </p>
-        </Box>
-      </div>
+      <Box
+        icon={<GiPencilBrush size={70} />}
+        size1={styles.large}
+        size2={styles.mediumLarge}
+        reveal={styles2.reveal}
+      >
+        <h3>Art</h3>
+        <p className={styles2.text}>
+          As an art enthusiast, drawing is, to me, an expression of my
+          creativity. I like to think of coding the same way as drawing, just
+          substituting a pen with letters and numbers.
+        </p>
+      </Box>
 
-      <div className={styles2.x}>
-        <Box
-          icon={<FaPeopleGroup size={70} />}
-          size1={styles.large}
-          size2={styles.mediumLarge}
-          reveal={styles2.reveal}
-        >
-          <h3>Management</h3>
-          <p className={styles2.text}>
-            I've supervised 150+ individuals on Australian solar plant projects,
-            highlighting the importance of teamwork to me.
-          </p>
-        </Box>
-      </div>
+      <Box
+        icon={<FaPeopleGroup size={70} />}
+        size1={styles.large}
+        size2={styles.mediumLarge}
+        reveal={styles2.reveal}
+      >
+        <h3>Management</h3>
+        <p className={styles2.text}>
+          I've supervised 150+ individuals on Australian solar plant projects,
+          highlighting the importance of teamwork to me.
+        </p>
+      </Box>
 
-      <div className={styles2.x}>
-        <Box
-          icon={<FaSkiing size={70} />}
-          size1={styles.large}
-          size2={styles.mediumLarge}
-          reveal={styles2.reveal}
-        >
-          <h3>Sport</h3>
-          <p className={styles2.text}>
-            Sports hold a special place in my heart, with skiing being a true
-            passion. During my younger years, I even competed as a
-            semi-professional skier.
-          </p>
-        </Box>
-      </div>
+      <Box
+        icon={<FaSkiing size={70} />}
+        size1={styles.large}
+        size2={styles.mediumLarge}
+        reveal={styles2.reveal}
+      >
+        <h3>Sport</h3>
+        <p className={styles2.text}>
+          Sports hold a special place in my heart, with skiing being a true
+          passion. During my younger years, I even competed as a
+          semi-professional skier.
+        </p>
+      </Box>
     </div>
   );
 }
