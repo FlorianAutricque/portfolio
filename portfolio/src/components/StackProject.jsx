47268af78a5e13fd9f40f629c948b5b544727010
florian autricque <fautricque@gmail.com>
2023-09-07 12:49:10 -0400

    design project page

---
+++ portfolio/src/components/StackProject.jsx
@@ -0,0 +1,75 @@
+import { useLocation } from "react-router-dom";
+
+import { FaReact, FaBootstrap } from "react-icons/fa6";
+import { BiLogoCss3, BiLogoJavascript } from "react-icons/bi";
+import { AiFillHtml5 } from "react-icons/ai";
+import { SiRubyonrails, SiTailwindcss, SiStimulus } from "react-icons/si";
+
+import LanguagesBox from "./LanguagesBox";
+
+import styles3 from "../pages/Languages.module.css";
+import styles from "./StackProject.module.css";
+
+function StackProject() {
+  const location = useLocation();
+
+  const project = location.state ? location.state.projectData : null;
+  return (
+    <>
+      <h3 className={styles.StackUsedText}>Stack used</h3>
+      <div className={styles.StackProjectGrid}>
+        {project.stack.slice(1).map((x, index) => (
+          <div key={index} className={styles.StackProject}>
+            {x === "CSS" ? (
+              <div className={styles3.containerEachBox}>
+                <LanguagesBox icon={<BiLogoCss3 size={80} />}>CSS</LanguagesBox>
+              </div>
+            ) : x === "HTML" ? (
+              <div className={styles3.containerEachBox}>
+                <LanguagesBox icon={<AiFillHtml5 size={80} />}>
+                  HTML
+                </LanguagesBox>
+              </div>
+            ) : x === "React" ? (
+              <div className={styles3.containerEachBox}>
+                <LanguagesBox icon={<FaReact size={80} />}>React</LanguagesBox>
+              </div>
+            ) : x === "Javascript" ? (
+              <div className={styles3.containerEachBox}>
+                <LanguagesBox icon={<BiLogoJavascript size={80} />}>
+                  Javascript
+                </LanguagesBox>
+              </div>
+            ) : x === "Stimulus" ? (
+              <div className={styles3.containerEachBox}>
+                <LanguagesBox icon={<SiStimulus size={80} />}>
+                  Stimulus
+                </LanguagesBox>
+              </div>
+            ) : x === "Ruby on rails" ? (
+              <div className={styles3.containerEachBox}>
+                <LanguagesBox icon={<SiRubyonrails size={80} />}>
+                  Rails
+                </LanguagesBox>
+              </div>
+            ) : x === "Bootstrap" ? (
+              <div className={styles3.containerEachBox}>
+                <LanguagesBox icon={<FaBootstrap size={80} />}>
+                  Bootstrap
+                </LanguagesBox>
+              </div>
+            ) : x === "Tailwind" ? (
+              <div className={styles3.containerEachBox}>
+                <LanguagesBox icon={<SiTailwindcss size={80} />}>
+                  Tailwindcss
+                </LanguagesBox>
+              </div>
+            ) : null}
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default StackProject;
--- portfolio/src/pages/Project.jsx
+++ portfolio/src/pages/Project.jsx
@@ -1,19 +1,13 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
-import PageNav from "../components/PageNav";
 import { AiFillGithub } from "react-icons/ai";
-
-import CarouselComponent from "../components/CarouselComponent";
-import LanguagesBox from "../components/LanguagesBox";
-
-import { FaReact, FaBootstrap } from "react-icons/fa6";
-import { BiLogoCss3, BiLogoJavascript } from "react-icons/bi";
-import { AiFillHtml5 } from "react-icons/ai";
-import { SiRubyonrails, SiTailwindcss, SiStimulus } from "react-icons/si";
 
 import styles2 from "../components/Button.module.css";
 import styles from "./Project.module.css";
-import styles3 from "./Languages.module.css";
+
+import PageNav from "../components/PageNav";
+import StackProject from "../components/StackProject";
+import CarouselComponent from "../components/CarouselComponent";
 
 function Project() {
   const location = useLocation();
@@ -28,61 +22,18 @@ function Project() {
 
         <div className={styles.containerImgText}>
           <div className={styles.stackGithub}>
-            {/* <strong>Stack used:</strong> {project.stack.slice(1).join(", ")} */}
-            <strong>Stack used:</strong>{" "}
-            {project.stack.slice(1).map((x, index) => (
-              <div key={index}>
-                {x === "CSS" ? (
-                  <div className={styles3.containerEachBox}>
-                    <LanguagesBox icon={<BiLogoCss3 size={80} />}>
-                      CSS
-                    </LanguagesBox>
-                  </div>
-                ) : x === "HTML" ? (
-                  <div className={styles3.containerEachBox}>
-                    <LanguagesBox icon={<AiFillHtml5 size={80} />}>
-                      HTML
-                    </LanguagesBox>
-                  </div>
-                ) : x === "React" ? (
-                  <div className={styles3.containerEachBox}>
-                    <LanguagesBox icon={<FaReact size={80} />}>
-                      React
-                    </LanguagesBox>
-                  </div>
-                ) : x === "Javascript" ? (
-                  <div className={styles3.containerEachBox}>
-                    <LanguagesBox icon={<BiLogoJavascript size={80} />}>
-                      Javascript
-                    </LanguagesBox>
-                  </div>
-                ) : x === "Stimulus" ? (
-                  <div className={styles3.containerEachBox}>
-                    <LanguagesBox icon={<SiStimulus size={80} />}>
-                      Stimulus
-                    </LanguagesBox>
-                  </div>
-                ) : x === "Ruby on rails" ? (
-                  <div className={styles3.containerEachBox}>
-                    <LanguagesBox icon={<SiRubyonrails size={80} />}>
-                      Rails
-                    </LanguagesBox>
-                  </div>
-                ) : x === "Bootstrap" ? (
-                  <div className={styles3.containerEachBox}>
-                    <LanguagesBox icon={<FaBootstrap size={80} />}>
-                      Bootstrap
-                    </LanguagesBox>
-                  </div>
-                ) : x === "Tailwind" ? (
-                  <div className={styles3.containerEachBox}>
-                    <LanguagesBox icon={<SiTailwindcss size={80} />}>
-                      Tailwindcss
-                    </LanguagesBox>
-                  </div>
-                ) : null}
-              </div>
-            ))}
+            <StackProject />
+          </div>
+
+          <div className={styles.aboutDescription}>
+            <h3>About </h3>
+            {project
+              ? project.description
+                  .split("\n")
+                  .map((paragraph, index) => <p key={index}>{paragraph}</p>)
+              : ""}{" "}
+            <br />
+            <br />
             <Link
               to={project.github}
               target="_blank"
@@ -92,16 +43,6 @@ function Project() {
                 <AiFillGithub size={20} /> Github
               </span>
             </Link>
-          </div>
-
-          <div className={styles.aboutDescription}>
-            <h3>About: </h3>
-
-            {project
-              ? project.description
-                  .split("\n")
-                  .map((paragraph, index) => <p key={index}>{paragraph}</p>)
-              : ""}
           </div>
         </div>
 
