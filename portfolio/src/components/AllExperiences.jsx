26e318944e003938ea29a0d5da2c4bb8ed210935
florian autricque <fautricque@gmail.com>
2024-01-20 18:30:01 -0500

    new style for experience page

--- portfolio/src/components/AllExperiences.jsx
+++ portfolio/src/components/AllExperiences.jsx
@@ -56,6 +56,36 @@ const Experiences = () => {
         t("experienceExp.3.3"),
       ],
     },
+    {
+      title: "Team Leader",
+      date: t("date.6"),
+      location: "All Energy Contracting",
+      location2: [<CiLocationOn />, t("localisationExp.2")],
+      experience: [t("experienceExp.4.1"), t("experienceExp.4.2")],
+    },
+    {
+      title: t("expTitle.3"),
+      date: t("date.7"),
+      location: "Protech Group",
+      location2: [<CiLocationOn />, t("localisationExp.3")],
+      experience: [
+        t("experienceExp.5.1"),
+        t("experienceExp.5.2"),
+        t("experienceExp.5.3"),
+        t("experienceExp.5.4"),
+      ],
+    },
+    {
+      title: t("expTitle.3"),
+      date: t("date.8"),
+      location: "Bamford Engineering & Consulting",
+      location2: [<CiLocationOn />, t("localisationExp.3")],
+      experience: [
+        t("experienceExp.6.1"),
+        t("experienceExp.6.2"),
+        t("experienceExp.6.3"),
+      ],
+    },
   ];
   return (
     <>
@@ -107,164 +137,6 @@ const Experiences = () => {
     </>
   );
 };
-
-// function Experiences({ title, date, location, experience }) {
-//   return (
-//     (
-//       <>
-//         <Scroll position={styles.first}>
-//           <h3>{t(title)}</h3>
-//           <h5>{t(date)}</h5>
-//           <p>
-//             <em>{location}</em>
-//           </p>
-//           <br />
-//           {t(experience)}
-//         </Scroll>
-//       </>
-//     ),
-//     {
-//       /* <Scroll position={styles.first}>
-//         <h3>{t("expTitle.12")}</h3>
-//         <h5>{t("date.15")}</h5>
-//         <p>
-//           <em>Montreal</em>
-//         </p>
-//         <br />
-//         {t("experienceExp.13")}
-//       </Scroll> */
-//     },
-//     {
-//       /* <div className={styles.margin}>
-//         <Scroll position={styles.right}>
-//           <h3>{t("expTitle.11")}</h3>
-//           <h5>{t("date.14")}</h5>
-//           <p>
-//             <em>Udemy</em>
-//           </p>
-//           <br />
-//           {t("experienceExp.12")}
-//         </Scroll>
-//       </div> */
-//     },
-//     {
-//       /* <Scroll position={styles.left}>
-//         <h3>{t("expTitle.9")}</h3>
-//         <h5>{t("date.1")}</h5>
-//         <p>
-//           <em>Udemy</em>
-//         </p>
-//         <br />
-//         React.js, Tailwind CSS, supabase, Redux.js, JavaScript eXtension (JSX),
-//         CSS Modules, Styled Components, Context API, React Router.
-//       </Scroll> */
-//     },
-//     {
-//       /* <div className={styles.margin}>
-//         <Scroll position={styles.right}>
-//           <h3>{t("expTitle.10")}</h3>
-//           <h5>{t("date.2")}</h5>
-//           <p>
-//             <em>Udemy</em>
-//           </p>
-//           <br />
-//           Modern OOP, Asynchronous JavaScript, NPM, Parcel, Babel and ES6
-//           modules.
-//         </Scroll>
-//       </div> */
-//     },
-//     {
-//       /* <Scroll position={styles.left}>
-//         <h3>{t("expTitle.8")}</h3>
-//         <h5>{t("date.3")}</h5>
-//         <p>
-//           <em>Le Wagon Montreal</em>
-//         </p>
-//         <br />
-//         {t("experienceExp.1")}
-//       </Scroll> */
-//     },
-//     {
-//       /* <div className={styles.margin}>
-//         <Scroll position={styles.right}>
-//           <h3>{t("expTitle.1")}</h3>
-//           <h5>{t("date.4")}</h5>
-//           <p>
-//             <em>France</em>
-//           </p>
-//           <br />
-//           {t("experienceExp.2")}
-//         </Scroll>
-//       </div> */
-//     },
-//     {
-//       /* <Scroll position={styles.left}>
-//         <h3>{t("expTitle.2")}</h3>
-//         <h5>{t("date.5")}</h5>
-//         <p>
-//           <em>Stralis Energy</em>
-//         </p>
-//         <span>
-//           <CiLocationOn /> {t("localisationExp.1")}
-//         </span>
-//         <br /> <br />
-//         {t("experienceExp.3.1")} <br />
-//         {t("experienceExp.3.2")}
-//         <br />
-//         {t("experienceExp.3.3")}
-//       </Scroll> */
-//     },
-//     {
-//       /* <div className={styles.margin}>
-//         <Scroll position={styles.right}>
-//           <h3>Team Leader</h3>
-//           <h5>{t("date.6")}</h5>
-//           <p>
-//             <em>All Energy Contracting</em>
-//           </p>
-//           <span>
-//             <CiLocationOn /> {t("localisationExp.2")}
-//           </span>{" "}
-//           <br /> <br />
-//           {t("experienceExp.4.1")} <br /> {t("experienceExp.4.2")}
-//         </Scroll>
-//       </div>
-
-//       <Scroll position={styles.left}>
-//         <h3>{t("expTitle.3")}</h3>
-//         <h5>{t("date.7")}</h5>
-//         <p>
-//           <em>Protech Group</em>
-//         </p>
-//         <span>
-//           <CiLocationOn /> {t("localisationExp.3")}
-//         </span>{" "}
-//         <br /> <br /> {t("experienceExp.5.1")}
-//         <br />
-//         {t("experienceExp.5.2")}
-//         <br />
-//         {t("experienceExp.5.3")}
-//         <br />
-//         {t("experienceExp.5.4")}
-//       </Scroll>
-
-//       <div className={styles.margin}>
-//         <Scroll position={styles.right}>
-//           <h3>{t("expTitle.3")}</h3>
-//           <h5>{t("date.8")}</h5>
-//           <p>
-//             <em>Bamford Engineering & Consulting</em>
-//           </p>
-//           <span>
-//             <CiLocationOn /> {t("localisationExp.3")}
-//           </span>{" "}
-//           <br /> <br /> {t("experienceExp.6.1")}
-//           <br />
-//           {t("experienceExp.6.2")}
-//           <br />
-//           {t("experienceExp.6.3")}
-//         </Scroll>
-//       </div>
 
 //       <Scroll position={styles.left}>
 //         <h3>{t("expTitle.4")}</h3>
@@ -331,8 +203,5 @@ const Experiences = () => {
 //         <br />
 //         {t("experienceExp.11.5")}
 //       </Scroll> */
-//     }
-//   );
-// }
 
 export default Experiences;
