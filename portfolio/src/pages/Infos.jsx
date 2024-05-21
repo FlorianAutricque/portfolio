338d1ba0a0331792f37905330c24d1e460fa3042
florian autricque <fautricque@gmail.com>
2024-05-21 17:42:42 -0400

    adding annimations

--- portfolio/src/pages/Infos.jsx
+++ portfolio/src/pages/Infos.jsx
@@ -6,9 +6,67 @@ import PageNav from "../components/PageNav";
 import styles from "./Infos.module.css";
 import ButtonInfos from "../components/ButtonInfos";
 import AllFunFacts from "../components/AllFunFacts";
+import { useEffect } from "react";
+
+import { GiPencilBrush } from "react-icons/gi";
+import { FaPeopleGroup } from "react-icons/fa6";
+import { FaSkiing } from "react-icons/fa";
 
 function Infos() {
   const { t } = useTranslation();
+
+  const AllFunFacts = [
+    {
+      icon: <GiPencilBrush size={70} />,
+      title: "Art",
+      text: t("funfacts.1"),
+    },
+    {
+      icon: <FaPeopleGroup size={70} />,
+      title: "Management",
+      text: t("funfacts.2"),
+    },
+    {
+      icon: <FaSkiing size={70} />,
+      title: "Sport",
+      text: t("funfacts.3"),
+    },
+  ];
+
+  useEffect(() => {
+    const elementsToObserve = [
+      document.querySelector(`.${styles.containerTextOne}`),
+      document.querySelector(`.${styles.hyperlink}`),
+      document.querySelector(`.${styles.boxFunFact}:nth-child(1) `),
+      document.querySelector(`.${styles.boxFunFact}:nth-child(2) `),
+      document.querySelector(`.${styles.boxFunFact}:nth-child(3) `),
+    ];
+
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.5,
+    };
+
+    const observerCallback = (entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add(styles.show);
+          observer.unobserve(entry.target);
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(observerCallback, options);
+
+    elementsToObserve.forEach(element => {
+      if (element) observer.observe(element);
+    });
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
 
   return (
     <>
@@ -25,25 +83,40 @@ function Infos() {
           <CiLocationOn /> <u>{t("locationInfos")}</u> Montreal, Canada
           <br />
           <br />
-          <p
+          <div
+            className={styles.containerTextOne}
             dangerouslySetInnerHTML={{
               __html: t("textInfos.1"),
             }}
           />
           <br /> <br />
-          <p
-            className={styles.hyperlink}
+          <div
             dangerouslySetInnerHTML={{
               __html: t("textInfos.2", {
                 LeWagon:
                   '<a href="https://www.lewagon.com/fr" target="_blank">Le Wagon</a>',
               }),
             }}
-          />
+            className={styles.hyperlink}
+          ></div>
         </div>
       </div>
+
       <div className={styles.containerFunFact}>
-        <AllFunFacts />
+        {AllFunFacts.map((fact, index) => (
+          <div
+            key={index}
+            className={`${styles.boxFunFact} ${styles.large} ${styles.reveal}`}
+          >
+            <div className={`${styles.boxOutside} ${styles.mediumLarge}`}>
+              {fact.icon}
+              <h3>{fact.title}</h3>
+              <p className={styles.text}>{fact.text}</p>
+            </div>
+          </div>
+        ))}
+
+        {/* <AllFunFacts /> */}
       </div>
     </>
   );
