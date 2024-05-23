7f653ece5fcc1eba724b553a3d236d8b6fc83bb0
florian autricque <fautricque@gmail.com>
2024-05-22 21:29:43 -0400

    fix style

--- portfolio/src/pages/Project.jsx
+++ portfolio/src/pages/Project.jsx
@@ -1,6 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import React, { useEffect } from "react";
 
 import styles from "./Project.module.css";
 import styles2 from "../components/Button.module.css";
@@ -22,42 +21,6 @@ function Project() {
     });
   };
 
-  //animation
-
-  // useEffect(() => {
-  //   const elementToObserver = [
-  //     document.querySelector(`.${styles.containerDescriptionProject}`),
-  //     document.querySelector(`.${styles.containerCarouselComponent}`),
-  //   ];
-
-  //   const options = {
-  //     root: null,
-  //     rootMargin: "0px",
-  //     threshold: 0.5,
-  //   };
-
-  //   const observerCallBack = (entries, observer) => {
-  //     entries.forEach((entry, index) => {
-  //       if (entry.isIntersecting) {
-  //         setTimeout(() => {
-  //           entry.target.classList.add(styles.show);
-  //           observer.unobserve(entry.target);
-  //         }, index * 200);
-  //       }
-  //     });
-  //   };
-
-  //   const observer = new IntersectionObserver(observerCallBack, options);
-
-  //   elementToObserver.forEach(el => {
-  //     if (el) observer.observe(el);
-  //   });
-
-  //   return () => {
-  //     observer.disconnect();
-  //   };
-  // }, []);
-
   return (
     <>
       <PageNav />
@@ -65,14 +28,11 @@ function Project() {
         <h1>{project ? project.name : ""}</h1>
         <div className={styles.containerMain}>
           <StackProject />
-          {/* <div className={styles.containerDescriptionProject}> */}
+
           <DescriptionProject />
-          {/* </div> */}
         </div>
 
-        {/* <div className={styles.containerCarouselComponent}> */}
         <CarouselComponent />
-        {/* </div> */}
 
         <div className={styles.goBack}>
           <Link to="/projects" className={styles2.button} onClick={scrollToTop}>
